0fe7c2fcc12692540f52f11a6aa34ab9ccf11ffe
mousemouse0894 <biohazardking2012@gmail.com>
2019-06-26 22:44:30 +0700

    Update1

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-home',
@@ -6,7 +9,64 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public imageSelect: any = null;
+  public imageProcess: any = null;
 
-  constructor() {}
+  constructor(private camera:Camera,private http:HttpClient) {}
+  public onBack() {
+    
+  }
+  public deleteimage(){
+    null;
+  }
+  public onOpencamera() {
+    const options: CameraOptions = {
+      quality: 10,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      correctOrientation: true
+    };
+
+    this.camera.getPicture(options).then(
+      imageData => {
+        this.imageSelect = imageData;
+        this.http
+          .post(
+            "https://api.openalpr.com/v2/recognize_bytes?recognize_vehicle=1&country=th&secret_key=sk_9ee63276d1d00be2f697491b",
+            this.imageSelect
+          )
+          .subscribe((res: any) => {
+            this.imageProcess = res;
+          });
+      },
+      err => {}
+    );
+  }
+  public onOpengallery() {
+    const options: CameraOptions = {
+      quality: 10,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      correctOrientation: true,
+      sourceType: this.camera.PictureSourceType.SAVEDPHOTOALBUM
+    };
+
+    this.camera.getPicture(options).then(
+      imageData => {
+        this.imageSelect = imageData;
+        this.http
+          .post(
+            "https://api.openalpr.com/v2/recognize_bytes?recognize_vehicle=1&country=th&secret_key=sk_9ee63276d1d00be2f697491b",
+            this.imageSelect
+          )
+          .subscribe((res: any) => {
+            this.imageProcess = res;
+          });
+      },
+      err => {}
+    );
+  }
 
 }
